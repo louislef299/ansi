2f388ab64031494295718139e4c52c0071cd98de
Louis Lefebvre <louislefebvre1999@gmail.com>
2023-10-15 14:00:11 -0500

    add three stages for testing

--- main.go
+++ main.go
@@ -19,45 +19,30 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//runStages(height)
+	runStages(height)
 
 	fmt.Printf("the terminal width is %d and the height is %d\n", width, height)
 }
 
-func runTicker() {
-	fmt.Println("program start")
-	ticker := time.Tick(time.Second)
-	for i := 1; i <= 5; i++ {
-		<-ticker
-		fmt.Printf("\rOn %d/5", i)
-	}
-	fmt.Printf("\rthis should be deleted")
-	fmt.Printf("\rAll is said and done.\n")
-	fmt.Printf("\033[1A")
-}
-
 func runStages(bufSize int) {
-	stage1 := []string{
-		"hello louis",
-		"hello joe",
-		"hello cash",
-		"hello zach",
-	}
-
 	buff := buffer{
-		bufferSize: bufSize - 2,
+		bufferSize: bufSize - 5,
 	}
 
-	ticker := time.Tick(time.Millisecond * 200)
-	for i := 0; i < 50; i++ {
-		part := i % len(stage1)
-		<-ticker
-		buff.print(stage1[part])
-	}
-
+	runSampleStage(buff)
 	time.Sleep(time.Second)
 	eraseLines(buff.bufferSize)
 	fmt.Println("stage one finished!")
+
+	runSampleStage(buff)
+	time.Sleep(time.Second)
+	eraseLines(buff.bufferSize)
+	fmt.Println("stage two finished!")
+
+	runSampleStage(buff)
+	time.Sleep(time.Second)
+	eraseLines(buff.bufferSize)
+	fmt.Println("stage three finished!")
 }
 
 func (b *buffer) print(output string) {
@@ -78,4 +63,32 @@ func eraseLines(lines int) {
 		fmt.Printf("\033[%dA\r", 1)
 		fmt.Printf("\033[2K")
 	}
+}
+
+func runSampleStage(buff buffer) {
+	stage1 := []string{
+		"hello louis",
+		"hello joe",
+		"hello cash",
+		"hello zach",
+	}
+
+	ticker := time.Tick(time.Millisecond * 200)
+	for i := 0; i < 50; i++ {
+		part := i % len(stage1)
+		<-ticker
+		buff.print(stage1[part])
+	}
+}
+
+func runTicker() {
+	fmt.Println("program start")
+	ticker := time.Tick(time.Second)
+	for i := 1; i <= 5; i++ {
+		<-ticker
+		fmt.Printf("\rOn %d/5", i)
+	}
+	fmt.Printf("\rthis should be deleted")
+	fmt.Printf("\rAll is said and done.\n")
+	fmt.Printf("\033[1A")
 }
