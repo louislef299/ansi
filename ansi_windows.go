b3200a60689221a6a9d95781ab0d181b90ef2939
Louis Lefebvre <lefebl4@medtronic.com>
2023-10-25 13:34:15 -0700

    docs: Remove color ref

--- ansi_windows.go
+++ ansi_windows.go
@@ -10,7 +10,7 @@ import (
 )
 
 func init() {
-	// Opt-in for ansi color support for current process.
+	// Opt-in for ansi support for current process.
 	// https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#output-sequences
 	var outMode uint32
 	out := windows.Handle(os.Stdout.Fd())
